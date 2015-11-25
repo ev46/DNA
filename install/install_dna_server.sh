5bc91e7661426e2501be903336c71bab873dc5b3
gennady1 <gennady.staskevich@gmail.com>
2015-11-25 17:04:14 -0500

    added server side script for dna server

--- install/install_dna_server.sh
+++ install/install_dna_server.sh
@@ -13,3 +13,19 @@ tar xzf redis-stable.tar.gz
 cd redis-stable/
 
 sudo make install
+
+cd ~
+
+mkdir -p ./gocode/src/github.com/dnastat
+
+cd gocode/src/github.com/dnastat/
+
+git init
+
+git pull https://github.com/ev46/dnastat.git
+
+go get github.com/mediocregopher/radix.v2/redis
+
+cd ~/gocode/src/github.com/dnastat/server
+
+go install
