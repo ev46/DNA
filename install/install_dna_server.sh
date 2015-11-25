3ddecedb4312595e5c26f926bd4ad90e99cdd338
gennady1 <gennady.staskevich@gmail.com>
2015-11-25 15:18:33 -0500

    fixed copy and paste error in script

--- install/install_dna_server.sh
+++ install/install_dna_server.sh
@@ -2,7 +2,9 @@ mkdir tmp
 
 cd tmp
 
-wget​http://download.redis.io/redis­stable.tar.gz tar xzf​r​edis­stable.tar.gz
+wget​http://download.redis.io/redis­stable.tar.gz
+
+tar xzf​r​edis­stable.tar.gz
 
 cd ~/tmp/redis­stable
 
