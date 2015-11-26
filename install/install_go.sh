3ec607ea99998829b326273a56304d8f83ecd342
gennady1 <gennady.staskevich@gmail.com>
2015-11-25 22:29:55 -0500

    dealing with script

--- install/install_go.sh
+++ install/install_go.sh
@@ -10,15 +10,14 @@ wget https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
 
 sudo tar -C /usr/local -xzf ~/tmp/go1.5.1.linux-amd64.tar.gz
 
-sudo sudo echo "
+sudo echo "
 #go install
-export PATH=$PATH:/usr/local/go/bin
+export PATH=\$PATH:/usr/local/go/bin
 
 # Run compiled gocode from any directory
-export GOPATH=$HOME/gocode
-export PATH=$PATH:$GOPATH/bin
+export GOPATH=\$HOME/gocode
+export PATH=\$PATH:\$GOPATH/bin
 
 " >> ~/.profile
 
 source ~/.profile
-
